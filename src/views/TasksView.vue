99427e884b89cfbf5ed0401a5551ac7ccbbce509
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-04-03 15:32:16 +0200

    improve tasks look and feel using tailwind

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -8,12 +8,9 @@ import { useUserStore } from '@/stores/userStore'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-
-// const account = ref(null)
 const tasksStore = useTasksStore()
 const { tasks } = storeToRefs(tasksStore)
 const userStore = useUserStore()
-
 const { user } = storeToRefs(userStore)
 
 const logOut = async () => {
@@ -27,14 +24,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <main>
-    <h1>Tasks view</h1>
+  <main class="container mx-auto">
+    <h1 class="text-3xl font-semibold mb-4">Your tasks</h1>
     <template v-if="user">
       <CreateTask />
     </template>
     <template v-if="tasks && tasks.length">
-      <span>Total tasks: {{ tasks.length }}</span>
-      <div clas="tasks-container">
+      <p class="mb-2">Total tasks: {{ tasks.length }}</p>
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         <TaskCard v-for="task in tasks" :key="task.id" :task="task" />
       </div>
     </template>
@@ -43,8 +40,10 @@ onMounted(() => {
       <p v-else-if="!tasks">Loading tasks...</p>
       <p v-else>No tasks available.</p>
     </template>
-    <button @click="logOut()">Logout</button>
+    <button @click="logOut()" class="mt-4 bg-red-500 text-white px-4 py-2 rounded">Logout</button>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
