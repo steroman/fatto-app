1e58e06dc779af3d7865c461210f04128ec2dd98
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-04-06 17:12:40 +0200

    throw error message in a toast when users haven't verified email on login

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -2,8 +2,9 @@
 import { useUserStore } from '@/stores/userStore'
 import { useVuelidate } from '@vuelidate/core'
 import { required, email } from '@vuelidate/validators'
-import { reactive, computed } from 'vue'
+import { ref, reactive, computed } from 'vue'
 import { useRouter } from 'vue-router'
+import ToastComponent from '@/components/ToastComponent.vue'
 
 const router = useRouter()
 
@@ -12,6 +13,8 @@ const userStore = useUserStore()
 // const signIn = () => {
 //   userStore.signIn(form.email, form.password)
 // }
+
+const errorMessage = ref('')
 
 const form = reactive({
   email: '',
@@ -32,8 +35,24 @@ const rules = computed(() => {
 const v$ = useVuelidate(rules, form)
 
 const signInAndRedirect = async () => {
-  await userStore.signIn(form.email, form.password)
-  router.push('/tasks')
+  try {
+    await userStore.signIn(form.email, form.password)
+    router.push('/tasks')
+  } catch (error) {
+    if (error.message === 'Email not confirmed') {
+      errorMessage.value = 'This email isn\'t verified yet. Check your inbox and try to login again.'
+      resetErrorMessageAfterDelay(3000) // 3000 milliseconds (3 seconds)
+    } else {
+      errorMessage.value = 'An error occurred. Please try again.'
+      resetErrorMessageAfterDelay(3000) // 3000 milliseconds (3 seconds)
+    }
+  }
+}
+
+function resetErrorMessageAfterDelay(delay) {
+  setTimeout(() => {
+    errorMessage.value = ''
+  }, delay)
 }
 
 async function handleSubmit() {
@@ -65,10 +84,37 @@ async function handleSubmit() {
         </div>
         <button type="submit" class="bg-blue-500 text-white px-4 py-2">Log in</button>
         </form>
+        <div class="toast toast-top toast-center" v-if="errorMessage">
+          <div class="toast-container">
+      <transition name="fade">
+        <ToastComponent :errorMessage="errorMessage" />
+      </transition>
+    </div>
+    </div>
         <p class="mt-4">Don't have an account? <router-link to="/signup" class="text-blue-500">Sign up</router-link></p>
         <p><router-link to="/forgot-password" class="text-blue-500">Forgot password?</router-link></p>
     </div>
 </template>
 
 <style scoped>
+.alert {
+  gap: 0;
+}
+
+.toast-container {
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 50; /* Ensure the toast appears above other content */
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s; /* Smooth transition for opacity change */
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0; /* Start with 0 opacity during enter and leave transitions */
+}
+
 </style>
