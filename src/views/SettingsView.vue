a65d328eeb07133fd07d26559bcb487fced65c34
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-04-11 20:47:45 +0200

    create new view and add toggle

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="flex flex-col">
+  <div class="form-control w-52">
+    <label class="cursor-pointer label">
+      <span class="label-text">Hide completed tasks</span> 
+      <input type="checkbox" class="toggle toggle-accent" checked />
+    </label>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -46,6 +46,7 @@ onMounted(async () => {
       <p v-else>No tasks available.</p>
     </template>
     <button @click="logOut()" class="mt-4 bg-red-500 text-white px-4 py-2 rounded">Logout</button>
+    <button><router-link to="/settings" class="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Settings</router-link></button>
   </main>
 </template>
 
