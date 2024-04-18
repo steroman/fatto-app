698b7d23b73c7852037435adb59f2cd9f66b9d84
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-04-18 20:07:17 +0200

    use editModal to rename task

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -4,9 +4,9 @@
       <div :class="['font-semibold', task.is_complete ? 'text-gray-500' : 'text-lg']">{{ task.title }}</div>
       <div class="flex items-center">
         <!-- Edit button -->
-        <button @click="_openEditModal" class="focus:outline-none mr-2">
-  <img src="@/assets/icons/pencil-solid.svg" alt="Edit Task" class="w-4">
-</button>
+        <button @click="openEditModal" class="focus:outline-none mr-2">
+          <img src="@/assets/icons/pencil-solid.svg" alt="Edit Task" class="w-4">
+        </button>
 
         <!-- Completed button -->
         <button @click="_handleTaskCompletion" class="focus:outline-none">
@@ -18,27 +18,14 @@
       <div :class="['text-xs', task.is_complete ? 'text-gray-500' : 'text-gray-700']">Created on:<br/>{{ formatTimestamp(task.inserted_at) }}</div>
       <button @click="_deleteTask" class="text-red-500"><i class="fa-solid fa-trash-can"></i></button>
     </div>
-    <!-- Edit Modal -->
-    <dialog ref="editModal" class="modal modal-bottom sm:modal-middle">
-      <div class="modal-box">
-        <h3 class="font-bold text-lg">Edit Task Title</h3>
-        <input v-model="editedTitle" type="text" class="input my-4" placeholder="Enter new title">
-        <div class="modal-action">
-          <button @click="_saveTitle" class="btn mr-2">Save</button>
-          <button @click="_cancelEdit" class="btn btn-ghost">Cancel</button>
-        </div>
-      </div>
-    </dialog>
   </div>
 </template>
 
 <script setup>
-// Import necessary functions and libraries
-import { defineProps, ref } from 'vue'
+import { defineEmits } from 'vue'
 import { useTasksStore } from '@/stores/tasksStore'
 
-// Initialize tasks store
-const tasksStore = useTasksStore()
+const emit = defineEmits(['editTask'])
 
 // Define props for the component
 const props = defineProps({
@@ -48,9 +35,8 @@ const props = defineProps({
   }
 })
 
-// Initialize a reactive reference to the task
-const editedTitle = ref('')
-const editModal = ref(null)
+// Initialize tasks store
+const tasksStore = useTasksStore()
 
 // Function to toggle task completion status
 const _handleTaskCompletion = async () => {
@@ -59,27 +45,6 @@ const _handleTaskCompletion = async () => {
     is_complete: !props.task.is_complete
   }
   await tasksStore.updateExistingTask(taskToUpdate)
-}
-
-// Function to open edit modal
-const _openEditModal = () => {
-  editedTitle.value = props.task.title
-  editModal.value.showModal()
-}
-
-// Function to save edited title
-const _saveTitle = async () => {
-  const taskToUpdate = {
-    ...props.task,
-    title: editedTitle.value
-  }
-  await tasksStore.updateExistingTask(taskToUpdate)
-  editModal.value.close()
-}
-
-// Function to cancel edit
-const _cancelEdit = () => {
-  editModal.value.close()
 }
 
 // Function to delete the task
@@ -99,6 +64,12 @@ const getIconClass = () => {
   } else {
     return 'fa-check-circle text-gray-500'
   }
+}
+
+// Emit event to open edit modal
+const openEditModal = () => {
+  // Emit event with the task data
+  emit('editTask', props.task)
 }
 </script>
 
