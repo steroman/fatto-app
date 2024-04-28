8c6e87c6276b5b23b1fa52ef9f1c039ff0cb45a0
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-04-28 16:49:08 +0200

    add tailwind custom params

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#50C2C9',
+        secondary: '#F0F4F3',
+        hover: '#35969C'
+      },
+      height: {
+        taskWrap: 'calc(100% - 160px)',
+        taskWrap1: 'calc(100% - 104px)',
+        settings: 'calc(100% - 128px)',
+        settings1: 'calc(100% - 24px)'
+      },
+      maxWidth: {
+        120: '30rem',
+        160: '40rem',
+        taskWrap1: 'calc(100% - 104px)'
+      }
+    }
   },
-  plugins: [require("daisyui")],
+  plugins: []
 }
-
