a220609f189eda91f8ac68531f35bc795dfd7216
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-04-29 19:27:01 +0200

    clean navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,57 +1,56 @@
 <template>
-  <nav
-    class="flex items-center justify-between flex-wrap bg-primary dark:bg-gray-700 fixed w-full px-6 pt-4 pb-2 shadow-md z-30"
+<nav class="flex items-center justify-between flex-wrap dark:bg-gray-700 fixed w-full px-6 pt-4 pb-2 z-30">
+  <div class="flex items-center w-full pb-2 justify-between h-12">
+    <div class="flex items-center space-x-4">
+      <img src="@/assets/logo.svg" class="w-10 h-10 sm:mx-auto" />
+      <span class="font-semibold text-xl tracking-tight hidden sm:block">DoToo</span>
+    </div>
+    <button
+      @click="onMenuClick"
+      v-if="!isSettings"
+      class="bg-transparent hover:text-gray-100 hover:bg-hover dark:hover:bg-gray-800 rounded-full w-10 h-10 p-2"
+    >
+      <i class="material-icons"> more_vert </i>
+    </button>
+    <button
+      @click="onLogoutClick"
+      :class="[
+        isSettings ? 'sm:block md:block lg:block' : '',
+        'hidden w-20 h-10 bg-white text-primary dark:text-white hover:bg-gray-200 hover:text-hover dark:bg-gray-800 dark:hover:bg-gray-600 rounded-md'
+      ]"
+    >
+      Log out
+    </button>
+  </div>
+  <div
+    :class="[
+      'bg-white dark:bg-gray-800 dark:shadow-gray-600 w-28 block fixed right-6 top-16 shadow-md p-2 rounded-md z-50'
+    ]"
+    v-show="isOpen"
   >
-    <div class="flex items-center w-full pb-2 justify-between text-white h-12">
-      <span :class="['font-semibold text-xl tracking-tight']">DoToo</span>
+    <div class="w-full">
       <button
-        @click="onMenuClick"
-        v-if="!isSettings"
-        :class="[
-          'bg-transparent text-white hover:text-gray-100 hover:bg-hover dark:hover:bg-gray-800 rounded-full w-10 h-10 p-2'
-        ]"
+        @click="onSorybyClick"
+        class="w-full px-4 text-left h-10 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md block sm:hidden md:hidden lg:hidden"
       >
-        <i class="material-icons"> more_vert </i>
+        Sort by
+      </button>
+      <button
+        @click="onSettingsClick"
+        class="w-full px-4 text-left h-10 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md"
+      >
+        Settings
       </button>
       <button
         @click="onLogoutClick"
-        :class="[
-          isSettings ? 'sm:block md:block lg:block' : '',
-          'hidden w-20 h-10 bg-white text-primary dark:text-white hover:bg-gray-200 hover:text-hover dark:bg-gray-800 dark:hover:bg-gray-600 rounded-md'
-        ]"
+        class="w-full px-4 text-left h-10 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md"
       >
         Log out
       </button>
     </div>
+  </div>
+</nav>
 
-    <div
-      :class="[
-        'bg-white dark:bg-gray-800 dark:shadow-gray-600 w-28 block fixed right-6 top-16 shadow-md p-2 rounded-md z-50'
-      ]"
-      v-show="isOpen"
-    >
-      <div class="w-full">
-        <button
-          @click="onSorybyClick"
-          class="w-full px-4 text-left h-10 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md block sm:hidden md:hidden lg:hidden"
-        >
-          Sort by
-        </button>
-        <button
-          @click="onSettingsClick"
-          class="w-full px-4 text-left h-10 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md"
-        >
-          Settings
-        </button>
-        <button
-          @click="onLogoutClick"
-          class="w-full px-4 text-left h-10 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md"
-        >
-          Log out
-        </button>
-      </div>
-    </div>
-  </nav>
   <div class="block sm:hidden">
     <div
       class="w-screen h-screen fixed bg-gray-500 bg-opacity-75 top-0 left-0 z-50"
