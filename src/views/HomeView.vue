8adcf7e4e52a3e11b57fb734002fa8eae13550f6
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-04-30 21:50:29 +0200

    add logo to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,11 @@
 <template>
   <div class="pt-20 max-w-96 p-7 mx-auto">
     <h1 class="text-3xl font-bold text-center mb-4">
+      <img src="@/assets/logo.svg" alt="Fatto Logo" class="w-64 mx-auto mb-8" />
+
       Get things done with <span class="text-primary font-black">Fatto</span>
     </h1>
     <p class="text-lg text-center mb-4">Your simple and effective to-do list app.</p>
-    <img src="@/assets/home-illustration.svg" alt="Fatto Logo" class="w-64 mx-auto mb-8" />
     <router-link
       to="/signup"
       class="w-full h-14 block bg-primary dark:bg-gray-700 hover:bg-hover dark:hover:bg-gray-600 text-white hover:text-white rounded-lg text-center font-semibold text-xl p-3 hover:shadow-md"
