0611e0d44ee40f228844cab27516e73ed5654ad8
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-05-01 11:16:41 +0200

    longer limit for truncation

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -62,7 +62,7 @@ const props = defineProps({
 })
 
 const formatTitle = (title) => {
-  return title.length > 15 ? `${title.substring(0, 15)} ...` : title
+  return title.length > 25 ? `${title.substring(0, 25)} …` : title
 }
 
 const emit = defineEmits(['edit'])
