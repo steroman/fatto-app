deccaa0dd1c7ff85ad82172ddb09e04c69b692cd
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-05-01 09:47:38 +0200

    working resetpasswordview

--- src/views/ResetPasswordView.vue
+++ src/views/ResetPasswordView.vue
@@ -1,94 +1,90 @@
 <template>
-  <div class="pt-28 px-6 max-w-120 mx-auto w-full h-screen" v-if="!confirmationSent">
-    <h1 class="text-3xl font-bold text-center mb-4">Password forgotten</h1>
-    <p class="text-lg text-center mb-8">
-      Enter your email address and we'll send you a link to reset your password.
-    </p>
-    <form @submit.prevent="handleSubmit" class="mb-4">
-      <div class="space-y-1">
-        <label class="font-medium text-lg text-left w-full block" for="email">Email *</label>
-        <input
-          type="text"
-          v-model="form.email"
-          id="email"
-          :class="`rounded-md bg-white dark:text-gray-900 w-full h-12 text-sm px-6 py-4 outline-none ${v$.email.$error ? 'outline-red-300' : ''} ${v$.email.$error ? 'focus:outline-red-600' : 'focus:outline-primary'}`"
-        />
-        <span v-if="v$.email.$error" class="block text-red-500 text-sm text-left">{{
-          v$.email.$errors[0].$message
-        }}</span>
+  <div class="pt-28 px-6 max-w-120 mx-auto w-full h-screen">
+      <div v-if="!confirmationSent">
+          <h1 class="text-2xl font-bold text-center mb-4">Update password</h1>
+          <form @submit.prevent="handleSubmit">
+              <div class="space-y-4 mb-6">
+                  <div class="space-y-1">
+                      <label class="font-medium text-lg text-left w-full block" for="password">New password *</label>
+                          <input type="password" v-model="form.password" id="password"
+                              :class="`rounded-md bg-white dark:text-gray-900 w-full h-12 text-sm px-6 py-4 outline-none ${v$.password.$error ? 'outline-red-300' : ''} ${v$.password.$error ? 'focus:outline-red-600' : 'focus:outline-primary'}`"/>
+                          <span v-if="v$.password.$error" class="block text-red-500 text-sm text-left">{{ v$.password.$errors[0].$message}}</span>
+                      </div>
+                  <div class="space-y-1">
+                      <label class="font-medium text-lg text-left w-full block" for="newpassword">Confirm new password *</label>
+                      <input type="password" v-model="form.password1" id="newpassword"
+                          :class="`rounded-md bg-white dark:text-gray-900 w-full h-12 text-sm px-6 py-4 outline-none ${v$.password1.$error ? 'outline-red-300' : ''} ${v$.password1.$error ? 'focus:outline-red-600' : 'focus:outline-primary'}`"/>
+                      <span v-if="v$.password1.$error" class="block text-red-500 text-sm text-left">{{ v$.password1.$errors[0].$message }}</span>
+                  </div>
+              </div>
+              <button
+                  class="w-full h-14 mt-6 bg-primary hover:bg-hover text-white hover:text-white rounded-lg text-center font-semibold text-xl p-3 hover:shadow-md">Update password</button>
+              <span :class="['block text-red-500 text-sm text-left', success ? 'invisible' : 'visible']">{{message}}</span>
+          </form>
       </div>
-
-      <button
-        type="submit"
-        class="w-full h-14 mt-10 bg-primary dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-hover text-white hover:text-white rounded-lg text-center font-semibold text-xl p-3 hover:shadow-md"
-      >
-        Send Link
-      </button>
-      <span
-        :class="['block text-red-500 text-sm text-center', success ? 'invisible' : 'visible']"
-        >{{ message }}</span
-      >
-      <span v-if="v$.email.$error" class="block text-red-500 text-sm text-center">Fix errors and try again.</span>
-    </form>
-
-    <router-link to="/login" class="text-primary hover:text-hover">Back to log in</router-link>
-  </div>
-  <div class="pt-20 md:max-w-96 mx-auto lg:max-w-96 w-10/12 sm:w-8/12" v-if="confirmationSent">
-    <h2 class="text-xl font-semibold mt-8">Check your inbox</h2>
-    <p class="text-lg mt-4 mb-5">
-      If an account associated with that email exists, you'll receive an email to help you reset
-      your password shortly.
-    </p>
-    <router-link to="/" class="text-primary hover:text-hover">Go back home</router-link>
+      <div v-if="confirmationSent">
+          <h1 class="text-3xl font-bold text-center mb-4">Password updated</h1>
+          <p class="text-lg text-center mb-8">You're ready to get back to your tasks.</p>
+          <router-link to="/tasks"
+              class="w-full h-14 block bg-primary hover:bg-hover text-white hover:text-white rounded-lg text-center font-semibold text-xl p-3 hover:shadow-md">Go to tasks</router-link>
+      </div>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive, computed } from 'vue'
-import { useVuelidate } from '@vuelidate/core'
-import { required, email, helpers } from '@vuelidate/validators'
 import { useUserStore } from '@/stores/userStore'
+import { useVuelidate } from '@vuelidate/core'
+import { helpers, minLength, required, sameAs } from '@vuelidate/validators'
 
 const userStore = useUserStore()
 
-const form = reactive({
-  email: ''
-})
+const confirmationSent = ref(false)
 
-const message = ref('')
-const success = ref(false)
+const message = ref("")
+const success = ref(true)
+
+
+const form = reactive({
+  password: '',
+  password1: ''
+})
 
 const rules = computed(() => {
   return {
-    email: {
-      required: helpers.withMessage('Email address cannot be empty', required),
-      email: helpers.withMessage('Enter correct email address', email)
-    }
+      password: {
+          required: helpers.withMessage('Password cannot be empty', required),
+          minLength: helpers.withMessage('Password must be longer than 6 characters', minLength(6))
+      }, // Password is required
+      password1: {
+          required: helpers.withMessage('Cofirm password cannot be empty', required),
+          sameAsPassword: helpers.withMessage('Retype password', sameAs(form.password))
+      } // Password confirmation is required
   }
 })
 
 const v$ = useVuelidate(rules, form)
 
-const confirmationSent = ref(false)
-
-const resetPassword = async () => {
+const updatePassword = async () => {
   try {
-    await userStore.resetPassword(form.email)
-    confirmationSent.value = true
-    success.value = true
+      await userStore.updateUserData({ password: form.password })
+      confirmationSent.value = true
   } catch (err) {
-    message.value = err.message
-    success.value = false
+      message.value = err.message
+      success.value = false
   }
 }
 
-async function handleSubmit() {
+const handleSubmit = async () => {
+  success.value = true
   const result = await v$.value.$validate()
   if (result) {
-    resetPassword()
+      success.value = true
+      updatePassword()
   } else {
-    message.value = 'Fix errors and try again.'
-    success.value = false
+      message.value = "Fix errors and try again."
+      success.value = false
   }
 }
+
 </script>
