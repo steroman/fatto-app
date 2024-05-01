3c2e0b3b6c82728198fcc2613b03be69dbb3b547
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-05-01 09:46:26 +0200

    replace empty state illustration with branded one

--- src/components/TasksEmptyState.vue
+++ src/components/TasksEmptyState.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="rounded-md w-full h-max px-4 pt-40 pb-8">
-    <img src="@/assets/home-illustration.svg" class="w-20 w-80 mx-auto mt-10 mb-5" />
+    <img src="@/assets/logo.svg" class="w-20 w-80 mx-auto mt-10 mb-5" />
     <p class="font-semibold text-2xl mb-5">No tasks left</p>
     <p class="text-lg">Create a new task or just enjoy some free time.</p>
   </div>
