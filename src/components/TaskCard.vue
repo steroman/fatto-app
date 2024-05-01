ea2269b750518aae0af0f9c2694c92ebb29271de
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-05-01 11:19:44 +0200

    min width of card to 350px

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,56 +1,3 @@
-<template>
-  <div
-    :class="[
-      'bg-white dark:bg-gray-600 dark:hover:shadow-gray-400 dark:shadow-sm rounded-md shadow-md hover:shadow-xl px-4 py-5 h-fit',
-      task.is_complete ? 'line-through opacity-50' : ''
-    ]"
-  >
-    <div class="flex flex-row justify-between">
-      <div
-        class="text-primary font-semibold text-lg text-left max-w-taskWrap1 break-words hyphens-auto"
-      >
-        {{ formatTitle(task.title) }}
-      </div>
-      <div class="space-x-1 min-w-20">
-        <button
-          @click="openEditModal"
-          :class="[
-            'bg-transparent hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full w-8 h-8 p-1',
-            task.is_complete ? 'text-gray-500' : 'text-primary hover:text-hover'
-          ]"
-        >
-          <i class="material-icons">edit</i>
-        </button>
-        <button
-          @click="handleTaskCompletion"
-          :class="[
-            'bg-transparent hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full w-8 h-8 p-1',
-            task.is_complete ? 'text-gray-500' : 'text-primary hover:text-hover'
-          ]"
-        >
-          <i class="material-icons">task_alt</i>
-        </button>
-      </div>
-    </div>
-    <div class="flex flex-row justify-between">
-      <div class="font-medium text-sm self-end w-10/12 text-wrap break-words text-left">
-        Created On : {{ formatTimestamp(task.inserted_at) }}
-      </div>
-      <div class="flex items-end">
-        <button
-          @click="deleteTask"
-          :class="[
-            'bg-transparent hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full w-8 h-8 p-1 mr-1',
-            task.is_complete ? 'text-gray-500' : 'text-primary hover:text-hover'
-          ]"
-        >
-          <i class="material-icons">delete</i>
-        </button>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script setup>
 import { useTasksStore } from '@/stores/tasksStore'
 
@@ -90,3 +37,56 @@ const openEditModal = () => {
   emit('edit', props.task)
 }
 </script>
+
+<template>
+  <div
+    :class="[
+      'bg-white dark:bg-gray-600 dark:hover:shadow-gray-400 dark:shadow-sm rounded-md shadow-md hover:shadow-xl px-4 py-5 h-fit min-w-[350px]',
+      task.is_complete ? 'line-through opacity-50' : ''
+    ]"
+  >
+    <div class="flex flex-row justify-between">
+      <div
+        class="text-primary font-semibold text-lg text-left max-w-taskWrap1 break-words hyphens-auto"
+      >
+        {{ formatTitle(task.title) }}
+      </div>
+      <div class="space-x-1 min-w-20">
+        <button
+          @click="openEditModal"
+          :class="[
+            'bg-transparent hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full w-8 h-8 p-1',
+            task.is_complete ? 'text-gray-500' : 'text-primary hover:text-hover'
+          ]"
+        >
+          <i class="material-icons">edit</i>
+        </button>
+        <button
+          @click="handleTaskCompletion"
+          :class="[
+            'bg-transparent hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full w-8 h-8 p-1',
+            task.is_complete ? 'text-gray-500' : 'text-primary hover:text-hover'
+          ]"
+        >
+          <i class="material-icons">task_alt</i>
+        </button>
+      </div>
+    </div>
+    <div class="flex flex-row justify-between">
+      <div class="font-medium text-sm self-end w-10/12 text-wrap break-words text-left">
+        Created on: {{ formatTimestamp(task.inserted_at) }}
+      </div>
+      <div class="flex items-end">
+        <button
+          @click="deleteTask"
+          :class="[
+            'bg-transparent hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full w-8 h-8 p-1 mr-1',
+            task.is_complete ? 'text-gray-500' : 'text-primary hover:text-hover'
+          ]"
+        >
+          <i class="material-icons">delete</i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
