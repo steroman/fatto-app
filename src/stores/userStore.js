04bbad9cb29fbcb4393b6e0d129a5545b68b38d2
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-05-03 11:46:11 +0200

    comment userStore

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,3 +1,4 @@
+// Importing necessary functions and variables from Vue and Pinia, as well as various API functions
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import {
@@ -8,7 +9,7 @@ import {
   updateUser,
   resetPassWithEmail
   // changePass
-} from '@/api/userApi'
+} from '@/api/userApi' // Importing user-related API functions
 import {
   fetchSortingPreference,
   updateSortingPreference,
@@ -16,28 +17,30 @@ import {
   updateHideCompletedSetting,
   fetchDarkModeSetting,
   updateDarkModeSetting
-} from '@/api/settingsApi'
+} from '@/api/settingsApi' // Importing settings-related API functions
 
+// Defining the user store
 export const useUserStore = defineStore('user', {
   state: () => {
     // State
-    const user = ref(null)
-    const sortingPreference = ref('')
-    const hideCompletedSetting = ref(false)
-    const displayName = ref('')
-    const isDarkMode = ref(false)
-    // Getters
+    const user = ref(null) // A reference to hold the current user information
+    const sortingPreference = ref('') // A reference to hold the user's sorting preference
+    const hideCompletedSetting = ref(false) // A reference to hold the hide completed tasks setting
+    const displayName = ref('') // A reference to hold the user's display name
+    const isDarkMode = ref(false) // A reference to hold the user's dark mode setting
+
+    // Getters (none at the moment)
 
     // Actions
-    async function createUser(email, password, displayName) {
+    async function createUser(email, password, displayName) { // Function to create a new user account
       try {
         await createAccount(email, password, displayName)
       } catch (error) {
-        console.error(error)
+        console.error(error) 
       }
     }
 
-    async function signIn(email, password) {
+    async function signIn(email, password) { // Function to sign in using the provided credentials
       try {
         user.value = await login(email, password)
       } catch (error) {
@@ -46,7 +49,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function seeUser() {
+    async function seeUser() { // Function to retrieve current user information
       try {
         user.value = await seeCurrentUser()
         displayName.value = user.value?.user_metadata?.display_name || ''
@@ -55,7 +58,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function signOut() {
+    async function signOut() { // Function to sign out the current user
       try {
         user.value = await logout()
       } catch (error) {
@@ -63,7 +66,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function updateUserData(userData) {
+    async function updateUserData(userData) { // Function to update user data
       try {
         const response = await updateUser(userData)
         user.value = response.user
@@ -74,7 +77,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function resetPassword(email) {
+    async function resetPassword(email) { // Function to initiate a password reset
       try {
         user.value = await resetPassWithEmail(email)
       } catch (error) {
@@ -82,7 +85,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function fetchUserSortingPreference() {
+    async function fetchUserSortingPreference() { // Function to fetch the user's sorting preference
       try {
         const sortingPrefFetched = await fetchSortingPreference(user.value.id)
         sortingPreference.value = sortingPrefFetched
@@ -91,7 +94,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function updateUserSortingPreference(newSortingPref) {
+    async function updateUserSortingPreference(newSortingPref) {  // Function to update the user's sorting preference
       try {
         const updatedSortingPref = await updateSortingPreference(user.value.id, newSortingPref)
         sortingPreference.value = updatedSortingPref
@@ -100,7 +103,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function fetchUserHideCompletedSetting() {
+    async function fetchUserHideCompletedSetting() { // Function to fetch the user's hide completed tasks setting
       try {
         const hideCompletedSettingFetched = await fetchHideCompletedSetting(user.value.id)
         hideCompletedSetting.value = hideCompletedSettingFetched
@@ -109,7 +112,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function updateUserHideCompletedSetting(newHideCompletedSetting) {
+    async function updateUserHideCompletedSetting(newHideCompletedSetting) { // Function to update the user's hide completed tasks setting
       try {
         const updatedHideCompletedSetting = await updateHideCompletedSetting(
           user.value.id,
@@ -121,7 +124,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function fetchDarkMode() {
+    async function fetchDarkMode() { // Function to fetch the user's dark mode setting
       try {
         const darkMode = await fetchDarkModeSetting(user.value.id)
         isDarkMode.value = darkMode
@@ -130,7 +133,7 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    async function updateDarkMode(newDarkMode) {
+    async function updateDarkMode(newDarkMode) { // Function to update the user's dark mode setting
       try {
         const updatedDarkMode = await updateDarkModeSetting(user.value.id, newDarkMode)
         isDarkMode.value = updatedDarkMode
@@ -139,41 +142,37 @@ export const useUserStore = defineStore('user', {
       }
     }
 
-    // async function changePassword(oldPass, newPass) {
-    //   await changePass(oldPass, newPass, user.value.id)
-    // }
-
     return {
       // State
-      user,
-      sortingPreference,
-      hideCompletedSetting,
-      displayName,
-      isDarkMode,
-      // Getters
+      user, // Exposing the user state
+      sortingPreference, // Exposing the sorting preference state
+      hideCompletedSetting, // Exposing the hide completed tasks setting state
+      displayName, // Exposing the display name state
+      isDarkMode, // Exposing the dark mode setting state
+
+      // Getters (none at the moment)
 
       // Actions
-      createUser,
-      signIn,
-      seeUser,
-      signOut,
-      updateUserData,
-      resetPassword,
-      fetchUserSortingPreference,
-      updateUserSortingPreference,
-      fetchUserHideCompletedSetting,
-      updateUserHideCompletedSetting,
-      fetchDarkMode,
-      updateDarkMode
-      // changePassword
+      createUser, // Exposing the createUser function
+      signIn, // Exposing the signIn function
+      seeUser, // Exposing the seeUser function
+      signOut, // Exposing the signOut function
+      updateUserData, // Exposing the updateUserData function
+      resetPassword, // Exposing the resetPassword function
+      fetchUserSortingPreference, // Exposing the fetchUserSortingPreference function
+      updateUserSortingPreference, // Exposing the updateUserSortingPreference function
+      fetchUserHideCompletedSetting, // Exposing the fetchUserHideCompletedSetting function
+      updateUserHideCompletedSetting, // Exposing the updateUserHideCompletedSetting function
+      fetchDarkMode, // Exposing the fetchDarkMode function
+      updateDarkMode // Exposing the updateDarkMode function
     }
   },
-  persist: {
-    enabled: true,
+  persist: { // Persisting the state using local storage
+    enabled: true, // Enabling persistence
     strategies: [
       {
-        key: 'user',
-        storage: localStorage
+        key: 'user', // Key for local storage persistence
+        storage: localStorage // Using local storage for persistence
       }
     ]
   }
