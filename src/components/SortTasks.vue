869d96a1f0cd9c756e0c64428aa9ca34895f62ef
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-05-03 11:28:45 +0200

    cleanup and comment TaskCard

--- src/components/SortTasks.vue
+++ src/components/SortTasks.vue
@@ -1,74 +1,76 @@
 <script setup>
+// Importing necessary functions and variables from other files
 import { SORT_OPTIONS, useTasksStore } from '@/stores/tasksStore'
 import { useUserStore } from '@/stores/userStore'
 import { onMounted, ref, watch } from 'vue'
 
+// Using the imported functions and variables
 const userStore = useUserStore()
 const tasksStore = useTasksStore()
-const selectedSort = ref(userStore.sortingPreference)
-const label = ref('')
-const show = ref(false)
+const selectedSort = ref(userStore.sortingPreference) // Creating a reactive reference for the selected sorting option
+const label = ref('') // Creating a reactive reference for the label to be displayed
+const show = ref(false) // Creating a reactive reference for the visibility of the sorting options menu
 
-// Function to toggle the visibility of the dropdown menu
+// Function to toggle the visibility of the sorting options menu
 const showMenu = (value) => {
   show.value = value
 }
 
+// Storing the available sorting options in a constant
 const options = SORT_OPTIONS
 
-// Function to apply sorting option
+// Function to apply the selected sorting option
 const applySorting = (value) => {
-  selectedSort.value = value
-  show.value = false
-  userStore.updateUserSortingPreference(selectedSort.value)
+  selectedSort.value = value // Setting the selected sorting option
+  show.value = false // Hiding the sorting options menu
+  userStore.updateUserSortingPreference(selectedSort.value) // Updating the user's sorting preference
 }
 
-// Watch for changes in the selected sorting option and update the label accordingly
+// Watching for changes in the selected sorting option
 watch(
   () => selectedSort.value,
   (newValue) => {
     SORT_OPTIONS.map((option) => {
       if (option.value === newValue) {
-        label.value = option.label
+        label.value = option.label // Updating the label based on the selected sorting option
       }
     })
   }
 )
 
-// Fetch tasks and set the initial selected sorting option on component mount
+// Executing the following code when the component is mounted
 onMounted(() => {
+  // Setting the initial label based on the selected sorting option
   SORT_OPTIONS.map((option) => {
     if (option.value === selectedSort.value) {
       label.value = option.label
     }
   })
   const userId = userStore.user.id
-  tasksStore.fetchAllTasks(userId)
-  selectedSort.value = userStore.sortingPreference
+  tasksStore.fetchAllTasks(userId) // Fetching all tasks for the user
+  selectedSort.value = userStore.sortingPreference // Initializing the selected sorting option based on the user's preference
 })
 </script>
 
 <template>
-  <!-- Dropdown menu for sorting tasks -->
+  <!-- Sorting options dropdown menu -->
   <div class="sm:block relative w-full hidden">
-    <!-- Dropdown toggle button -->
     <div
       @click="showMenu(!show)"
       class="block appearance-none w-full bg-white dark:bg-gray-700 border border-gray-400 hover:border-gray-500 px-4 py-3 text-left text-lg pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
     >
-      {{ label }} <!-- Display selected sorting option label -->
+      {{ label }} <!-- Displaying the current label of the selected sorting option -->
     </div>
 
-    <!-- Dropdown icon -->
+    <!-- Icon to indicate the dropdown functionality -->
     <div
       class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 dark:text-gray-300"
     >
       <i class="material-icons">keyboard_arrow_down</i>
     </div>
-    
-    <!-- Dropdown menu options -->
+    <!-- Dropdown menu for sorting options -->
     <div class="bg-white w-full absolute rounded-md z-40 shadow-md dark:bg-gray-700" v-if="show">
-      <!-- Iterate over sorting options -->
+      <!-- Iterating over the sorting options and displaying them -->
       <div
         @click="applySorting(option.value)"
         :selected="option.value === selectedSort"
@@ -80,7 +82,7 @@ onMounted(() => {
           selectedSort === option.value ? 'bg-primary dark:bg-gray-900 text-white' : ''
         ]"
       >
-        {{ option.label }} <!-- Display sorting option label -->
+        {{ option.label }} <!-- Displaying the label of each sorting option -->
       </div>
     </div>
   </div>
